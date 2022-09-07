207d5dbc9c1389dff0bc4acb2eccc47dc7eb5e68
alinen <alinen@savvysine.com>
2022-09-07 15:01:30 -0400

    week02 examples

---
+++ week02-sep5/CheckHeight1.java
@@ -0,0 +1,19 @@
+class CheckHeight1 {
+  public static void main(String[] args) {
+    System.out.print("Enter a height (inches): ");
+    int h = Integer.parseInt(System.console().readLine());
+
+    if (h > 60) {
+      println("Taller than 5 ft");
+    }
+    else if (h > 48) {
+      println("Taller than 4 ft");
+    }
+    else if (h > 36) {
+      println("Taller than 3 ft");
+    }
+    else {
+      println("Too small for this ride");
+    }
+  }
+};
---
+++ week02-sep5/Happy.java
@@ -0,0 +1,12 @@
+class Happy {
+    public static void main(String[] args) {
+        boolean isHappy = true;
+        boolean knowIt = false;
+        if (isHappy && knowIt) {
+            System.out.println("Clap your hands!");
+        }
+        else {
+            System.our.println("Sit quietly.");
+        }
+    }
+}
